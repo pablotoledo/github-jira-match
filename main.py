af2e6c87247e7369f0a37fd3d713ec834bc486d7
Pablo Toledo <pablotoledo@users.noreply.github.com>
2023-10-15 19:15:37 +0200

    Update main.py
    listados

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ github = Utils_Github()
 list_repos_jira = jira.get_repository_list()
 list_repos_github = github.get_list_repos()
 
+jira_repo_names = {item['repo_name'].lower() for item in list_repos_jira}
+github_repo_names = {item['repo_name'].lower() for item in list_repos_github}
+
+pending_create_jira = list(github_repo_names - jira_repo_names)
+pending_transition_delete = list(jira_repo_names - github_repo_names)
+
 print("")
